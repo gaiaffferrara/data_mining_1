34f6fb4e7e8ef5564316160248ab0a2f5e1874e1
Bazinga3 <b.barbieri7@studenti.unipi.it>
2024-10-23 18:08:49 +0200

    NaN handling genres

--- code/main.py
+++ code/main.py
@@ -7,10 +7,14 @@ from constants import numeric_columns as num_cols
 train_df = pd.read_csv('dm1_dataset_2425_imdb/train.csv')
 test_df = pd.read_csv('dm1_dataset_2425_imdb/test.csv')
 
-train_df[num_cols] = train_df[num_cols].apply(pd.to_numeric, errors='coerce')
+# train_df[num_cols] = train_df[num_cols].apply(
+    # pd.to_numeric, errors='coerce'
+    # )
 
 for col in num_cols:
     train_df[col] = pd.to_numeric(train_df[col], errors='coerce')
 # train_df[num_cols] = pd.to_numeric(train_df[num_cols], errors='coerce')
 
-train_df
+train_df.loc[train_df['genres'] == r'\N', 'genres'] = np.nan
+
+# train_df
