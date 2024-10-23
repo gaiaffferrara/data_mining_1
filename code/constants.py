6def5ba8c1774cf41acc8e4aba6dace8adc1f4a1
Bazinga3 <b.barbieri7@studenti.unipi.it>
2024-10-23 17:47:03 +0200

    NaN handling

---
+++ code/constants.py
@@ -0,0 +1,8 @@
+numeric_columns = [
+        'startYear', 'endYear', 'runtimeMinutes',
+        'awardWins', 'numVotes', 'worstRating',
+        'numVotes', 'worstRating', 'bestRating',
+        'totalImages', 'totalVideos', 'totalCredits',
+        'criticReviewsTotal', 'awardNominationsExcludeWins',
+        'numRegions', 'userReviewsTotal', 'ratingCount'
+    ]
---
+++ code/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+from constants import numeric_columns as num_cols
+
+
+train_df = pd.read_csv('dm1_dataset_2425_imdb/train.csv')
+test_df = pd.read_csv('dm1_dataset_2425_imdb/test.csv')
+
+train_df[num_cols] = train_df[num_cols].apply(pd.to_numeric, errors='coerce')
+
+for col in num_cols:
+    train_df[col] = pd.to_numeric(train_df[col], errors='coerce')
+# train_df[num_cols] = pd.to_numeric(train_df[num_cols], errors='coerce')
+
+train_df
